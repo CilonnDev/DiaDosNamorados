3c544bb0048a95d6ee1c9beb8ed5f2e190044495
Miguel Vitor <58395304+CilonnDev@users.noreply.github.com>
2025-06-11 01:30:41 -0300

    Add files via upload

--- js/index.js
+++
@@ -1,8 +0,0 @@
-const botao = document.querySelector("button");
-const h1 = document.querySelector("h1");
-
-botao.addEventListener("click", mudarTexto);
-
-function mudarTexto(){
-    h1.textContent = "Deu certo";
-}
